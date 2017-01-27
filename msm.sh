476a3ef426cef74f1f83b165c8012f43f273911d
Augusto Monteiro <augustomna2010@gmail.com>
2017-01-26 23:01:58 -0300

    Adding dependencies installation

--- msm.sh
+++ msm.sh
@@ -6,14 +6,14 @@ mycroft_virtualenv=~/.virtualenvs/mycroft/bin/activate
 
 echo "#######  Mycroft Skill Manager #######"
 
-function help {
+function help() {
 	echo ""
 	echo "1) help to see this menu"
 	echo "2) install <repository> to add a new skill"
 	echo ""
 }
 
-function install {
+function install() {
 	current=`pwd`
 	git_name=`echo "$2" | sed 's/.*\///'`
 	name=`echo "$git_name" | sed 's/.git//'`
@@ -34,6 +34,27 @@ function install {
         echo "Please restart you mycroft : D"
 		
 }
+
+# params: test command, apt name
+function test_and_install() {
+    eval $1 > /dev/null 2>&1
+    if [[ $? != 0 ]]; then
+        sudo apt-get -y install $2
+    fi
+}
+
+function install_dependencies() {
+    test_and_install "git --version" "git"
+    test_and_install "pip --version" "python-pip"
+}
+
+if [ -f ~/.msm ]; then
+    source ~/.msm
+fi
+if [ -z $MSM_DEPENDENCIES ]; then
+    install_dependencies
+    echo 'MSM_DEPENDENCIES=1' >> ~/.msm
+fi
 
 if [ "$1" = "install" ]; then
   install $*
